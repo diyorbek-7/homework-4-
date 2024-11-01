29565615859f9f512b071deb8c139fcfe07a558b
diyorbek-7 <d.shokirov@newuu.uz>
2024-11-01 16:38:25 +0500

    Add files via upload

---
+++ problem2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "problems.h"
+using namespace std;
+void sawp_values(double a, double b) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout <<"a="<< a << endl;
+    cout << "b="<<b << endl;
+}
+int main() {
+    double a, b;
+    cout<<"a="<<endl<<"b=";
+    cin >> a >> b;
+    sawp_values(a, b);
+    return 0;
+}
---
+++ problem3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "problems.h"
+using namespace std;
+int multiply(int a, int b) {
+    if (b == 0) {
+        return 0;
+    }
+    else {
+        return a + multiply(a, b-1);
+    }
+}
+int main() {
+    int a;
+    int b;
+    cin >> a;
+    cin >> b;
+    cout << multiply(a, b);
+}
